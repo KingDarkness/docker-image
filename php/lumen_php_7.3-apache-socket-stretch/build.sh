c645ca8d1d09503fee35c102fd0d6adeecea2e5f
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-05-06 14:43:01 +0700

    :boom:

--- php/lumen_php_7.3-apache-socket-stretch/build.sh
+++ php/lumen_php_7.3-apache-socket-stretch/build.sh
@@ -2,3 +2,7 @@
 docker image rm kingdarkness/lumen-php:7.3-apache-socket-stretch -f
 docker build -t kingdarkness/lumen-php:7.3-apache-socket-stretch -f Dockerfile .
 docker push kingdarkness/lumen-php:7.3-apache-socket-stretch
+
+docker image rm kingdarkness/lumen-php:7.3-apache-socket-stretch-opcache -f
+docker build -t kingdarkness/lumen-php:7.3-apache-socket-stretch-opcache -f opcache.Dockerfile .
+docker push kingdarkness/lumen-php:7.3-apache-socket-stretch-opcache
