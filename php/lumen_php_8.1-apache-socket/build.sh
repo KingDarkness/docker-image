088fcde5bebb7f796f8392fee9f6e16cb1ad4793
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2023-03-08 09:22:52 +0700

    :boom:

---
+++ php/lumen_php_8.1-apache-socket/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker image rm kingdarkness/lumen-php:8.1-apache-socket -f
+docker build -t kingdarkness/lumen-php:8.1-apache-socket -f Dockerfile .
+docker push kingdarkness/lumen-php:8.1-apache-socket
+
+docker image rm kingdarkness/lumen-php:8.1-apache-socket-opcache -f
+docker build -t kingdarkness/lumen-php:8.1-apache-socket-opcache -f opcache.Dockerfile .
+docker push kingdarkness/lumen-php:8.1-apache-socket-opcache
