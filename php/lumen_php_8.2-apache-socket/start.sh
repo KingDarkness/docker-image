ad53039f1b6bc3519e3d33dd8bc20fb5b1bae612
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2024-03-06 16:51:02 +0700

    :boom:

--- php/lumen_php_8.2-apache-socket/start.sh
+++ php/lumen_php_8.2-apache-socket/start.sh
@@ -4,54 +4,55 @@ set -e
 
 role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-local}
+migration=${AUTORUN_MIGRATION:-true}
 
 initScript=${INIT_SCRIPT}
 
 customCommand=${CUSTOM_COMMAND}
 
 if [ -n "$initScript" ]; then
-    sh $initScript
+	sh $initScript
 fi
 
-if [[ "$env" != "local" ]] && [[ "$role" == "app" ]]; then
-    echo "migrate"
-    (cd /var/www/html && php artisan migrate --force)
+if [[ "$env" != "local" ]] && [[ "$role" == "app" ]] && [[ "$migration" == "true" ]]; then
+	echo "migrate"
+	(cd /var/www/html && php artisan migrate --force)
 fi
 
 if [ "$role" = "app" ]; then
 
-    exec apache2-foreground
+	exec apache2-foreground
 
 elif [ "$role" = "queue" ]; then
 
-    echo "Running the queue..."
-    php /var/www/html/artisan queue:work --verbose --tries=3 --timeout=90 --stop-when-empty
+	echo "Running the queue..."
+	php /var/www/html/artisan queue:work --verbose --tries=3 --timeout=90 --stop-when-empty
 
 elif [ "$role" = "scheduler" ]; then
 
-    while [ true ]; do
-        php /var/www/html/artisan schedule:run --verbose --no-interaction &
-        sleep 60
-    done
+	while [ true ]; do
+		php /var/www/html/artisan schedule:run --verbose --no-interaction &
+		sleep 60
+	done
 
 elif [ "$role" = "rpc-server" ]; then
 
-    echo "Running the rpc..."
-    php /var/www/html/artisan rabbit:rpc-server
+	echo "Running the rpc..."
+	php /var/www/html/artisan rabbit:rpc-server
 
 elif [ "$role" = "work-queues-server" ]; then
 
-    echo "Running the work-queues..."
-    php /var/www/html/artisan rabbit:work-queues-server
+	echo "Running the work-queues..."
+	php /var/www/html/artisan rabbit:work-queues-server
 
 elif [ "$role" = "horizon" ]; then
 
-    echo "Running the horizon..."
-    php /var/www/html/artisan horizon
+	echo "Running the horizon..."
+	php /var/www/html/artisan horizon
 elif [[ -n "$customCommand" ]] && [[ "$role" == "command" ]]; then
-    $customCommand
+	$customCommand
 
 else
-    echo "Could not match the container role \"$role\""
-    exit 1
+	echo "Could not match the container role \"$role\""
+	exit 1
 fi
