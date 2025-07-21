70a2c7cb7b79eb4b00f69fb68e03e0281d11f084
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2025-07-21 17:06:15 +0700

    feat: add pocketbase & memcached php

--- pocketbase/build.sh
+++ pocketbase/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker image rm kingdarkness/pocketbase:0.20.5 -f
-docker build -t kingdarkness/pocketbase:0.20.5 -f Dockerfile .
-docker push kingdarkness/pocketbase:0.20.5
+docker image rm kingdarkness/pocketbase:0.28.4 -f
+docker build -t kingdarkness/pocketbase:0.28.4 -f Dockerfile .
+docker push kingdarkness/pocketbase:0.28.4
